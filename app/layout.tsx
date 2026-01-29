f23a0e5bcd1c394e99b3b9e9116ac59974008d94
zprovisionz <zprovisionz@gmail.com>
2026-01-28 23:15:37 -0500

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
